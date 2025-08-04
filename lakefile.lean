e2b5836c6aaa7a84466695cf444966e1f162544e
neoques2 <156274120+neoques2@users.noreply.github.com>
2025-08-04 17:21:28 -0500

    Simplify lakefile for build

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,8 @@
 import Lake
-import Lean.Elab.Facet
 open Lake DSL
 
 package exportExample
 
 @[default_target]
-lean_lib Export where
-  precompileModules := true
-  nativeFacets := fun _ => #[.sharedLib]
+lean_lib Export
 
